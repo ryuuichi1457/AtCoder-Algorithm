ddf5f3616f6d61486c34b039cbed04679ff9432b
ryuuichi <ryuhashi1457@gmail.com>
2024-10-18 18:23:33 +0900

    確率的素数判定法の追加

---
+++ Miller-Rabin-test.py
@@ -0,0 +1,30 @@
+import random
+def Miller_Rabin_Test(N,K=20):#Kは試行回数、一般に誤判定する確率は1/4^K　(初期値20だと1/1.2E+24ぐらい)
+    if N<2:
+        return False
+    if N%2==0:
+        return N==2  
+    if N==3:
+        return True  
+    tmp= N-1
+    c=0
+    while tmp%2==0:
+        c+=1
+        tmp//=2
+    d=tmp
+
+    for _ in range(K):
+        a=random.randint(2, N-2)
+        x=pow(a,d,N)
+        if x==1 or x==N-1:
+            continue
+        
+        #2^r*d
+        for _ in range(c-1):
+            x = pow(x,2,N)
+            if x==N-1:
+                break
+        else:
+            return False
+
+    return True
